06f679dd24e02bdbcf91b19444d5135ae8aec3fe
infinivis <romain.cerantola@heig-vd.ch>
2015-07-06 11:08:20 +0200

    info begin

---
+++ public/js/info/globals.js
@@ -0,0 +1,35 @@
+//globals for infos Screen
+var pressionG =0;
+
+var humiditeG=0;
+
+var tempG=0;
+
+var nbAbricotsSemaineG=0;
+
+var nbAbricotsJourG=0;
+
+var nbAbricotsCourantG=0;
+
+var nbPartieG=0;
+
+var timeG=0;
+
+var warningG=0;
+
+var lkmInstantG=27.5;
+
+var lkmPicG=0;
+
+var lkmMoyG=0;
+
+var rkmInstantG=0;
+
+var rkmPicG=0;
+
+var rkmMoyG=0;
+
+var ecartNoG=0;
+
+
+
